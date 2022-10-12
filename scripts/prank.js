a91f18011d771278e90074f9ae60adb6eb8a4af1
suriya <48148739+herharry@users.noreply.github.com>
2022-10-12 18:08:03 +0530

    Create prank.js

---
+++ scripts/prank.js
@@ -0,0 +1,10 @@
+for (let i = 0; i < 100; i++) {
+  NewTab();
+}
+
+
+function NewTab() {
+	alert('hi');
+//             window.open(
+//             "https://www.geeksforgeeks.org", "_blank");
+        }
